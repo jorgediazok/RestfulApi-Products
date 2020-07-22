5282be0d1d02de2635d98177bc06d787b598b80b
Jorge <jorgediazok@gmail.com>
2020-07-22 00:01:13 -0300

    changed some stuff in the routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,34 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
+const Product = require('../models/Product');
+
 router.get('/', (req, res) => {
   res.status(200).json({
     message: 'Handling GET Request to /products',
   });
 });
 
-router.post('/', (req, res) => {
-  const product = {
-    name: req.body.name,
-    price: req.body.price,
-  };
-  res.status(201).json({
-    message: 'Handling POST Request to /products',
-    createdProduct: product,
-  });
+router.post('/', async (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+    });
+    await product.save();
+    res.status(201).json({
+      message: 'Handling POST requests to/products',
+      createdProduct: product,
+    });
+  } catch {
+    (err) => {
+      console.log(err);
+      res.status(500).json({
+        message: 'ERROR',
+      });
+    };
+  }
 });
 
-router.get('/:productId', (req, res) => {
-  const id = req.params.productId;
-  if (id === 'special') {
-    res.status(200).json({
-      message: 'You discovered the special id',
-      id: id,
-    });
-  } else {
-    res.status(200).json({
-      message: 'You passed an ID',
-    });
+router.get('/:productId', async (req, res) => {
+  try {
+    let response = await Product.findById({});
+    res.status(200).json({ response });
+  } catch {
+    (err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    };
   }
 });
 
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+require('./database');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
@@ -11,18 +15,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //CORS
-app.use((res, req, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Acces-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(cors());
 
 //Routes that handle requests
 app.use('/products', productRoutes);
