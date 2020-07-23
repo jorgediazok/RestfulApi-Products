e03e1b71ea20f306cdf6fc599d6161c94bc66f67
Jorge <jorgediazok@gmail.com>
2020-07-23 13:23:06 -0300

    jwt added

--- database.js
+++ database.js
@@ -5,6 +5,7 @@ mongoose
   .connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then((db) => console.log('DB is connected!'))
   .catch((err) => console.log(err));
